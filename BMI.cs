eb15d9aa290ecd9c2062a3837156991ff6c6adcd
blank36 <34336427+blank36@users.noreply.github.com>
2018-07-26 09:56:12 +0800

    C# exercise BMI
    C# exercise BMI

---
+++ BMI.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace BMICalculator
+{
+    class BMITest
+    {
+        double  BMIA,BMIA2;
+
+     
+
+        public void ConditionMetricAndImperial(string inputString)
+        {
+
+            if (inputString == "M")
+            {
+                Console.WriteLine("Okay You Choose Metric");
+            }
+            else if (inputString == "I")
+            {
+                Console.WriteLine("Okay You Choose Imperial");
+            }
+            else
+            {
+                Console.Write("Wrong Input");
+                Console.ReadKey();
+            }
+        }
+
+        public void BMICompute(double Weight, double Height)
+        {
+            BMIA = (Weight / ((Height) * (Height)));
+            Console.WriteLine("Your BMIA is "+ BMIA);
+        }
+
+        public void BMIEvaluate(double Weight, double Height)
+       {    
+    
+            BMIA2 = (Weight / ((Height) * (Height)));
+            if(BMIA2 < 18.5)
+            {
+                Console.WriteLine("You are Underweight");
+                Console.ReadKey();
+            }
+            else if(BMIA2 > 18.5 && BMIA < 24.9)
+            {
+                Console.WriteLine("You are Normal");
+                Console.ReadKey();
+            }
+            else if(BMIA2 > 25 && BMIA < 29.9)
+            {
+                Console.WriteLine("You are Overweight");
+                Console.ReadKey();
+            }
+            else if (BMIA2 > 30)
+            {
+                Console.WriteLine("You are Obese");
+                Console.ReadKey();
+            }
+        }
+
+
+    }
+}
---
+++ BMICalculator.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+
+
+namespace BMICalculator
+{
+    
+    class BMICalculator
+    {
+        public static string InputString;
+        public static double Weight;
+        public static double Height;
+        public static void Main() {
+   
+        Console.WriteLine("Which Method do you Prefer Metric or Imperial \n Click (M) for Metric and (I) for Imperial ");
+        InputString = Console.ReadLine();                   
+        BMITest bmi = new BMITest();
+        bmi.ConditionMetricAndImperial(InputString);
+
+            if(InputString == "M" || InputString == "m")
+            {            
+                Console.WriteLine("Input your Meters:");
+                Convert.ToString(Height); 
+                Height = double.Parse(Console.ReadLine()); 
+             
+                Console.WriteLine("Input your Kilograms:");
+                Convert.ToDouble(Weight);
+                Weight = double.Parse(Console.ReadLine());
+            }
+            else if (InputString == "I" || InputString == "i")
+            {
+                Console.WriteLine("Input your Inches:");
+                Convert.ToString(Height);
+                Height = double.Parse(Console.ReadLine());
+     
+                Console.WriteLine("Input your Pounds:");
+                Convert.ToDouble(Weight);
+                Weight = double.Parse(Console.ReadLine());
+            }
+         bmi.BMICompute(Weight, Height);
+         bmi.BMIEvaluate(Weight, Height);
+
+        }
+
+
+       
+
+
+    }
+}
+
+
+
